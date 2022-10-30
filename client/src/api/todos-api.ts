4f521cc06ba2992edf165e4e2c185a5dae88e3cd
absaGH <absa3852@gmail.com>
2022-10-30 13:21:16 +0300

    minor changes

--- client/src/api/todos-api.ts
+++ client/src/api/todos-api.ts
@@ -27,7 +27,7 @@ export async function createTodo(
       'Authorization': `Bearer ${idToken}`
     }
   })
-  return response.data.item
+  return response.data.todo
 }
 
 export async function patchTodo(
